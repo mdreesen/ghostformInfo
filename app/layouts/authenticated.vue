f269aacf8e43f102b1b9065ed4d07cd645f877b8
Michael Dreesen <mdreesen@Mac.lan>
2026-05-12 13:25:56 -0600

    cleanup(logs)

--- app/layouts/authenticated.vue
+++ app/layouts/authenticated.vue
@@ -1,13 +1,12 @@
 <script setup lang="ts">
 await useFetch('/api/user', { key: 'get_user', lazy: true });
 const { data: user } = useNuxtData('get_user');
-console.log(user.value)
 </script>
 
 <template>
     <main>
         <template v-if="user">
-            <baseNavigationAuth v-if="user" />
+            <baseNavigationAuth v-if="user.paid" />
             <slot />
         </template>
 
